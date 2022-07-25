32e5429dbb76c5aa401e076c55648f9ba12629cc
Bruno Silva <brunosilva030202@gmail.com>
2022-07-25 14:12:32 -0300

    arrumando o link para deploy

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -3,7 +3,8 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+// const BASE_URL = 'http://localhost:3000/api' -> URL local
+const BASE_URL = 'https://curso-next-quiz.vercel.app/api' // -> URL do projeto em produção
 
 export default function Home() {
   const router = useRouter()
